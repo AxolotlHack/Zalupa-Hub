a85a7569a5d72da357e988aa68d344c8237cc65b
AxolotlHack <113066175+AxolotlHack@users.noreply.github.com>
2023-01-29 00:38:28 +0200

    Add files via upload

---
+++ Zalupa Hub.lua
@@ -0,0 +1,4 @@
+
+if game.PlaceId == 10972284553 then -- Skala Simulator
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Skala%20Simulator.lua'))()
+end
