811075df78f3ffa021eb781dab53af37802701f7
AxolotlHack <113066175+AxolotlHack@users.noreply.github.com>
2023-02-12 17:16:08 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 
 ## Game
 * Skala Simulator
+* Mine Racer
 
 ## Script
 ```
--- Zalupa Hub.lua
+++ Zalupa Hub.lua
@@ -1,4 +1,6 @@
-
-if game.PlaceId == 10972284553 then -- Skala Simulator
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Skala%20Simulator.lua'))()
+local PlaceId = game.PlaceId
+if PlaceId == 10972284553 then -- Skala Simulator
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Skala%20Simulator.lua"))()
+elseif PlaceId == 10898965735 then -- Mine Racer
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Mine%20Racer.lua"))()
 end
