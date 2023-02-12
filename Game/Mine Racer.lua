1755e358d8f6c6d9b7e11cf6e0a855a09f158aa8
AxolotlHack <113066175+AxolotlHack@users.noreply.github.com>
2023-02-12 17:13:05 +0200

    Update Mine Racer.lua

--- Game/Mine Racer.lua
+++ Game/Mine Racer.lua
@@ -1114,4 +1114,4 @@ local bb=game:service'VirtualUser'
 game:service'Players'.LocalPlayer.Idled:connect(function()
 bb:CaptureController()bb:ClickButton2(Vector2.new())
 end)
-print(Anti Afk / ON)
+print("Anti Afk / ON")
