1feefe6eaf0f9b7f8ee9fc0fce891e4ea9d51482
AxolotlHack <113066175+AxolotlHack@users.noreply.github.com>
2023-05-14 16:19:40 +0300

    Update Zalupa Hub.lua

--- Zalupa Hub.lua
+++ Zalupa Hub.lua
@@ -3,4 +3,6 @@ if PlaceId == 10972284553 then -- Skala Simulator
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Skala%20Simulator.lua"))()
 elseif PlaceId == 10898965735 then -- Mine Racer
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Mine%20Racer.lua"))()
+elseif PlaceId == 10704789056 then --Drive World
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AxolotlHack/Zalupa-Hub/main/Game/Drive%20World.lua"))()
 end
