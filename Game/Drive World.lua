d762a88c01b6959668cb60531518683e9d620c45
AxolotlHack <113066175+AxolotlHack@users.noreply.github.com>
2024-02-14 21:02:42 +0200

    Update Drive World.lua
    Choco farm

--- Game/Drive World.lua
+++ Game/Drive World.lua
@@ -1,13 +1,15 @@
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
-local main = Instance.new("Frame")
 local MainButton = Instance.new("TextButton")
-local Farm = Instance.new("Frame")
+local farm = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
-local FoodLabel = Instance.new("TextLabel")
 local TrailersLabel = Instance.new("TextLabel")
+local FoodLabel = Instance.new("TextLabel")
+local ChocoLabel = Instance.new("TextLabel")
 local TrailersButton = Instance.new("TextButton")
+local ChocoButton = Instance.new("TextButton")
 local FoodButton = Instance.new("TextButton")
+local main = Instance.new("Frame")
 local FarmButton = Instance.new("TextButton")
 
 --Properties:
@@ -15,39 +17,38 @@ local FarmButton = Instance.new("TextButton")
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Position = UDim2.new(0.345919609, 0, 0.278514564, 0)
-Frame.Size = UDim2.new(0, 252, 0, 333)
-Frame.Visible = true
 Frame.Active = true
-Frame.Draggable = true
-
-main.Name = "main"
-main.Parent = Frame
-main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-main.Position = UDim2.new(0.0158730168, 0, 0.0780780762, 0)
-main.Size = UDim2.new(0, 244, 0, 301)
-main.Visible = false
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.345999986, 0, 0.278514564, 0)
+Frame.Size = UDim2.new(0, 252, 0, 333)
 
 MainButton.Name = "MainButton"
 MainButton.Parent = Frame
 MainButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-MainButton.Position = UDim2.new(0.0158730168, 0, 0.00900900923, 0)
+MainButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+MainButton.BorderSizePixel = 0
+MainButton.Position = UDim2.new(0.0160000008, 0, 0.00900900923, 0)
 MainButton.Size = UDim2.new(0, 75, 0, 20)
 MainButton.Font = Enum.Font.SourceSans
 MainButton.Text = "Main"
 MainButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 MainButton.TextSize = 14.000
 
-Farm.Name = "Farm"
-Farm.Parent = Frame
-Farm.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Farm.Position = UDim2.new(0.0158730168, 0, 0.0780780762, 0)
-Farm.Size = UDim2.new(0, 244, 0, 301)
+farm.Name = "farm"
+farm.Parent = Frame
+farm.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+farm.BorderColor3 = Color3.fromRGB(0, 0, 0)
+farm.BorderSizePixel = 0
+farm.Position = UDim2.new(0.0160000008, 0, 0.0780780762, 0)
+farm.Size = UDim2.new(0, 244, 0, 301)
 
-TextLabel.Parent = Farm
+TextLabel.Parent = farm
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
 TextLabel.Position = UDim2.new(0.266393453, 0, 0, 0)
 TextLabel.Size = UDim2.new(0, 92, 0, 21)
 TextLabel.Font = Enum.Font.SourceSans
@@ -55,52 +56,96 @@ TextLabel.Text = "Auto Farm"
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.TextSize = 21.000
 
+TrailersLabel.Name = "TrailersLabel"
+TrailersLabel.Parent = farm
+TrailersLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TrailersLabel.BackgroundTransparency = 1.000
+TrailersLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TrailersLabel.BorderSizePixel = 0
+TrailersLabel.Position = UDim2.new(0, 0, 0.0697674453, 0)
+TrailersLabel.Size = UDim2.new(0, 163, 0, 26)
+TrailersLabel.Font = Enum.Font.SourceSans
+TrailersLabel.Text = "Deliver Trailers / OFF"
+TrailersLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TrailersLabel.TextSize = 20.000
+
 FoodLabel.Name = "FoodLabel"
-FoodLabel.Parent = Farm
+FoodLabel.Parent = farm
 FoodLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 FoodLabel.BackgroundTransparency = 1.000
+FoodLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+FoodLabel.BorderSizePixel = 0
 FoodLabel.Position = UDim2.new(0, 0, 0.156146184, 0)
-FoodLabel.Size = UDim2.new(0, 132, 0, 22)
+FoodLabel.Size = UDim2.new(0, 163, 0, 26)
 FoodLabel.Font = Enum.Font.SourceSans
 FoodLabel.Text = "Deliver Food / OFF"
 FoodLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 FoodLabel.TextSize = 20.000
 
-TrailersLabel.Name = "TrailersLabel"
-TrailersLabel.Parent = Farm
-TrailersLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TrailersLabel.BackgroundTransparency = 1.000
-TrailersLabel.Position = UDim2.new(0, 0, 0.0697674453, 0)
-TrailersLabel.Size = UDim2.new(0, 163, 0, 26)
-TrailersLabel.Font = Enum.Font.SourceSans
-TrailersLabel.Text = "Transport Trailers / OFF"
-TrailersLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TrailersLabel.TextSize = 20.000
+ChocoLabel.Name = "ChocoLabel"
+ChocoLabel.Parent = farm
+ChocoLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ChocoLabel.BackgroundTransparency = 1.000
+ChocoLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ChocoLabel.BorderSizePixel = 0
+ChocoLabel.Position = UDim2.new(0, 0, 0.242524922, 0)
+ChocoLabel.Size = UDim2.new(0, 163, 0, 26)
+ChocoLabel.Font = Enum.Font.SourceSans
+ChocoLabel.Text = "Deliver Choco / OFF"
+ChocoLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+ChocoLabel.TextSize = 20.000
 
 TrailersButton.Name = "TrailersButton"
-TrailersButton.Parent = Farm
+TrailersButton.Parent = farm
 TrailersButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-TrailersButton.Position = UDim2.new(0.709016383, 0, 0.0897009969, 0)
+TrailersButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TrailersButton.BorderSizePixel = 0
+TrailersButton.Position = UDim2.new(0.708999991, 0, 0.0897009969, 0)
 TrailersButton.Size = UDim2.new(0, 12, 0, 13)
 TrailersButton.Font = Enum.Font.SourceSans
 TrailersButton.Text = ""
 TrailersButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TrailersButton.TextSize = 14.000
 
+ChocoButton.Name = "ChocoButton"
+ChocoButton.Parent = farm
+ChocoButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+ChocoButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ChocoButton.BorderSizePixel = 0
+ChocoButton.Position = UDim2.new(0.708999991, 0, 0.262458473, 0)
+ChocoButton.Size = UDim2.new(0, 12, 0, 13)
+ChocoButton.Font = Enum.Font.SourceSans
+ChocoButton.Text = ""
+ChocoButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+ChocoButton.TextSize = 14.000
+
 FoodButton.Name = "FoodButton"
-FoodButton.Parent = Farm
+FoodButton.Parent = farm
 FoodButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-FoodButton.Position = UDim2.new(0.594262242, 0, 0.169435218, 0)
+FoodButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+FoodButton.BorderSizePixel = 0
+FoodButton.Position = UDim2.new(0.708999991, 0, 0.176079735, 0)
 FoodButton.Size = UDim2.new(0, 12, 0, 13)
 FoodButton.Font = Enum.Font.SourceSans
 FoodButton.Text = ""
 FoodButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 FoodButton.TextSize = 14.000
 
+main.Name = "main"
+main.Parent = Frame
+main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+main.BorderColor3 = Color3.fromRGB(0, 0, 0)
+main.BorderSizePixel = 0
+main.Position = UDim2.new(0.0160000008, 0, 0.0780780762, 0)
+main.Size = UDim2.new(0, 244, 0, 301)
+main.Visible = false
+
 FarmButton.Name = "FarmButton"
 FarmButton.Parent = Frame
 FarmButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-FarmButton.Position = UDim2.new(0.353174597, 0, 0.00900900923, 0)
+FarmButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+FarmButton.BorderSizePixel = 0
+FarmButton.Position = UDim2.new(0.353174597, 0, 0.00899999961, 0)
 FarmButton.Size = UDim2.new(0, 75, 0, 20)
 FarmButton.Font = Enum.Font.SourceSans
 FarmButton.Text = "Farm"
@@ -142,7 +187,7 @@ TrailersButton.MouseButton1Click:connect(function()
         getfenv().Trailers = true
 
         TrailersButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
-        TrailersLabel.Text = 'Transport Trailers / ON'
+        TrailersLabel.Text = 'Deliver Trailers / ON'
 
         while getfenv().Trailers do
             wait()
@@ -168,7 +213,7 @@ TrailersButton.MouseButton1Click:connect(function()
     else
         getfenv().Trailers = false
         TrailersButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-        TrailersLabel.Text = 'Transport Trailers / OFF'
+        TrailersLabel.Text = 'Deliver Trailers / OFF'
     end
 end)
 
@@ -210,5 +255,41 @@ FoodButton.MouseButton1Click:connect(function()
         getfenv().Food = false
         FoodButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
         FoodLabel.Text = 'Deliver Food / OFF'
+    end
+end)
+
+ChocoButton.MouseButton1Click:connect(function()
+    if getfenv().Trailers == false then
+
+        getfenv().Trailers = true
+
+        ChocoButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+        ChocoLabel.Text = 'Deliver Choco / ON'
+
+        while getfenv().Trailers do
+            wait()
+            if game:GetService("Players").LocalPlayer.PlayerGui.Score.Frame.Jobs.Visible == false then
+                wait(0.5)
+                game:GetService("ReplicatedStorage").Systems.Jobs.StartJob:InvokeServer("ChocolateDelivery", "EventLocation")
+                wait(10)
+            elseif game:GetService("Players").LocalPlayer.PlayerGui.Score.Frame.Jobs.Visible == true then
+                for i,v in pairs(game:GetService("Workspace").Cars:GetDescendants()) do
+                    if v.Name == "Owner" and v.Value == game.Players.LocalPlayer and game.Players.LocalPlayer:DistanceFromCharacter(game:GetService("Workspace").CompletionRegion.Primary.Position) > 25 then
+                        game:GetService("Workspace").CompletionRegion.Primary.Size=Vector3.new(100, 0.2, 100)
+                        v.Parent:SetPrimaryPartCFrame(game:GetService("Workspace").CompletionRegion.Primary.CFrame*CFrame.new(0,5,-25))
+                        wait(3)
+                    end
+                end
+                if game:GetService("Players").LocalPlayer.PlayerGui.JobComplete.Enabled == true then
+                    game:GetService("ReplicatedStorage").Systems.Jobs.CashBankedEarnings:FireServer()
+                    game:GetService("Players").LocalPlayer.PlayerGui.JobComplete.Enabled = false
+                    game:GetService("Lighting").BackgroundBlur.Enabled = false
+                end
+            end
+        end
+    else
+        getfenv().Trailers = false
+        ChocoButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+        ChocoLabel.Text = 'Deliver Choco / OFF'
     end
 end)
